0688793b96c9aea76aa5705ed9d4d08002d7c470
Daksh Jain <dakshjain1265@gmail.com>
2026-03-27 20:58:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of factors/number-of-factors.py
+++ Difficulty: Easy/Number of factors/number-of-factors.py
@@ -1,6 +1,6 @@
 class Solution:
     def countFactors (self, n):
-        count=1
+        count=1#checking extension
         for i in range(1,(n//2) +1):
             if n%i==0:
                 count=count+1

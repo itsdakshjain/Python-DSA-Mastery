8925166b8bfff81472b35d7e6fbfe5a9117015a8
Daksh Jain <dakshjain1265@gmail.com>
2026-05-02 15:37:03 +0530

    Time: 14 ms (79.25%) | Memory: 19.2 MB (94.10%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        count = 0        
+        for i in range(1, n + 1):
+            s = str(i)
+            if '3' in s or '4' in s or '7' in s:
+                continue
+            if '2' in s or '5' in s or '6' in s or '9' in s:
+                count += 1                
+        return count

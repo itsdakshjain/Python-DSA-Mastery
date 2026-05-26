f2d99d08041807bae640a107a5d1df1fb1ef26c6
Daksh Jain <dakshjain1265@gmail.com>
2026-05-26 20:22:25 +0530

    Time: 7 ms (2.01%) | Memory: 19.3 MB (25.31%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        unique_chars = set(word)
+        count = 0
+        
+        for i in range(26):
+            lower = chr(ord('a') + i)
+            upper = chr(ord('A') + i)
+            if lower in unique_chars and upper in unique_chars:
+                count += 1
+                
+        return count
